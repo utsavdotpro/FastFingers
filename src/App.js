f9592651e050ceaaf9d9de80ba4d695f9ac08d97
Utsav Barnwal <u.barnwal36@gmail.com>
2021-02-20 19:57:40 +0530

    add style utils and colors. add Home page and create basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from "./pages/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container max-w-screen-lg m-auto p-10 h-screen flex items-center">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="text-white shadow-xl bg-primary-dark rounded-xl p-5 h-96 w-full flex items-center flex-col justify-center">
+      <p className="text-4xl">Hi there!</p>
+      <br />
+      <p className="text-2xl">Ready to begin?</p>
+    </div>
+  );
+}
+
+export default Home;
