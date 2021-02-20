b9016a9c04639f66f2c015eaaceb00fbbe7a35b7
Utsav Barnwal <u.barnwal36@gmail.com>
2021-02-21 01:02:31 +0530

    minor change to Text component and using image as bg for container

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-function Text({ size = "", bold = false, color = "white", children }) {
+function Text({
+  size = "",
+  bold = false,
+  color = "white",
+  className: classes,
+  children,
+}) {
   size = size !== "" ? "text-" + size : size;
   color = color !== "" ? " text-" + color : color;
-  return <p className={size + color}>{bold ? <b>{children}</b> : children}</p>;
+  return (
+    <p className={size + color + " " + classes}>
+      {bold ? <b>{children}</b> : children}
+    </p>
+  );
 }
 
 export default Text;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,18 @@ import Text from "../../components/Text";
 
 function Home() {
   return (
-    <div className="text-white shadow-2xl bg-primary-dark rounded-xl p-5 h-full w-full flex items-center flex-col justify-center">
+    <div className="text-white rounded-xl bg-container p-5 h-full w-full flex items-center flex-col">
+      <Text size="xl" bold="true" className="mt-10">
+        00 : 00
+      </Text>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <Text size="4xl" bold="true">
         Hi there!
       </Text>
