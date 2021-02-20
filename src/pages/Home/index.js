2d5059f14db00326995da7d81af1b9ecc1ff391f
Utsav Barnwal <u.barnwal36@gmail.com>
2021-02-20 20:02:11 +0530

    minor improv to home layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="text-white shadow-xl bg-primary-dark rounded-xl p-5 h-96 w-full flex items-center flex-col justify-center">
+    <div className="text-white shadow-2xl bg-primary-dark rounded-xl p-5 h-full w-full flex items-center flex-col justify-center">
       <p className="text-4xl">Hi there!</p>
       <br />
-      <p className="text-2xl">Ready to begin?</p>
+      <p className="text-xl text-gray-200">Ready to begin your challenge?</p>
     </div>
   );
 }
