5766b8adc5ea390f0e0209d018e48b9610f43e8a
Utsav Barnwal <u.barnwal36@gmail.com>
2021-02-25 01:02:34 +0530

    wip: reducer in PlayContainer

--- src/pages/Game/containers/PlayContainer/index.js
+++ src/pages/Game/containers/PlayContainer/index.js
@@ -1,24 +1,70 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import Input, { useInput } from "../../../../components/Input";
 import Timer from "../../../../components/Timer";
 import Word from "../../../../components/Word";
 import { useDictionary } from "../../hooks";
 import { usePlayInput } from "./hooks";
 
-function PlayContainer({ difficulty, levelFactor }) {
-  const [word, setWord] = useState("");
+const UNIT_LEVEL_FACTOR = 0.01;
+
+const ACTIONS = {
+  UPDATE_WORD: "UPDATE_WORD",
+  UPDATE_LEVEL_FACTOR: "UPDATE_LEVEL_FACTOR",
+  UPDATE_DIFFICULTY: "UPDATE_DIFFICULTY",
+};
+
+const reducer = (state, { type, data = "" }) => {
+  switch (type) {
+    case ACTIONS.UPDATE_WORD:
+      return { ...state, word: data };
+    case ACTIONS.UPDATE_LEVEL_FACTOR:
+      return { ...state, levelFactor: data };
+
+    default:
+      return state;
+  }
+};
+
+function PlayContainer({ difficulty }) {
+  const [{ word, levelFactor }, dispatch] = useReducer(reducer, {
+    word: "",
+    levelFactor: 0,
+  });
+
+  const [text, setText] = useState("");
+
+  const handleTextChange = ({ target: { value } }) => {
+    // if (value.length < word.length) return;
+
+    if (word[value.length - 1] === value[value.length - 1]) setText(value);
+
+    if (value.length === word.length) handleWordComplete();
+  };
 
   const { getWord, setDifficulty } = useDictionary(difficulty);
 
-  useEffect(() => setWord(getWord()), [levelFactor]);
+  // const [word, setWord] = useState("");
 
-  const [text, handleTextChange, setToWordPlayInput] = usePlayInput(word);
+  useEffect(() => dispatch({ type: ACTIONS.UPDATE_WORD, data: getWord() }), [
+    levelFactor,
+  ]);
 
-  useEffect(() => setToWordPlayInput(word), [word]);
+  // const [text, handleTextChange, setWordToPlayInput] = usePlayInput(word);
 
-  const handleWordComplete = () => {};
+  // useEffect(() => setWordToPlayInput(word), [word]);
+
+  const handleWordComplete = () => {
+    setText("");
+    dispatch({ type: ACTIONS.UPDATE_WORD, data: getWord() });
+    dispatch({
+      type: ACTIONS.UPDATE_LEVEL_FACTOR,
+      data: levelFactor + UNIT_LEVEL_FACTOR,
+    });
+  };
 
   const handleWordFailed = () => {};
+
+  console.log("rendering...: ", levelFactor);
 
   return (
     <>
